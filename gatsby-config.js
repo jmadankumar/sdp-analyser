30e2d21fd176b97fb718ec15d9d8edc444d2090e
Madan Kumar <kumarmadan.j@gmail.com>
2020-07-16 15:05:13 +0530

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/sdp.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-172842755-1'
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
